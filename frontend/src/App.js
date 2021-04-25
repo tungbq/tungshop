e2763bced77d22e41bd8b4389328de37424f3f24
Tung Bui <tungbq.info@gmail.com>
2021-04-25 20:41:42 +0700

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Proshop</h1>
+    </>
+  );
+}
+
+export default App;
