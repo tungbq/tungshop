435b3103ebe76666d61cc13c36a93291fe81d4ac
Tung Bui <tungbq.info@gmail.com>
2021-05-11 22:54:43 +0700

    Chap 6: Cart screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -9,7 +9,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 
             if (existItem) {
                 return {
-                    ... state,
+                    ...state,
                     cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
                 }
             } else {
