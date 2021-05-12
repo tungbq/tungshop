13f4e6df67f4d3964e00c6591a54797e39e3dae2
Tung Bui <tungbq.info@gmail.com>
2021-05-12 23:32:33 +0700

    Chap 7 [Backend user auth] User authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 import products from './data/products.js'
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
@@ -12,11 +14,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
