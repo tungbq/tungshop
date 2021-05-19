2bae02d49ded4d87dfcce6ac8b3c8f812460db5c
Tung Bui <tungbq.info@gmail.com>
2021-05-19 20:38:09 +0700

    Chap 9: Payment screen and save payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
