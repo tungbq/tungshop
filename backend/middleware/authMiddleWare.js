01bb290899fdc16065cccb4be3dcb4fbab3fa500
Tung Bui <tungbq.info@gmail.com>
2021-05-19 21:41:16 +0700

    Chap 9: Place Order Screem

--- backend/middleware/authMiddleWare.js
+++ backend/middleware/authMiddleWare.js
@@ -5,8 +5,7 @@ import User from '../models/userModel.js'
 const protect = asyncHandler(async (req, res, next) => {
     let token
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
-    {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
