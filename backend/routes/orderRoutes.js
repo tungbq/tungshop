29dc669071c34069b4c9c996887279e61d4e3c08
Tung Bui <tungbq.info@gmail.com>
2021-05-20 21:18:39 +0700

    Chap 10: Update order to pay endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderControllers.js'
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderControllers.js'
 import { protect } from '../middleware/authMiddleWare.js'
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
