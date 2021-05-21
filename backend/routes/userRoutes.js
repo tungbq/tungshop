1d3b68ccc113b0e7628e489aea09c03c6ad826a7
Tung Bui <tungbq.info@gmail.com>
2021-05-21 17:02:02 +0700

    Chap 11: Getting a user by ID and updating user Endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,9 @@ import {
   registerUser,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleWare.js'
 
@@ -14,6 +16,9 @@ router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 router.route('/profile').put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
