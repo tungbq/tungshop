ba0c1f9ac0643d43ac9a83032a3f6c2d25c42c83
Tung Bui <tungbq.info@gmail.com>
2021-05-21 19:03:50 +0700

    Chap 12: Admin delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,7 +25,24 @@ const getProductById = asyncHandler(async (req, res) => {
     res.json(product)
 })
 
+// @desc    Delete product
+// @route   DELETE /api/product/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+    res.json(product)
+})
+
 export {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct
 }
