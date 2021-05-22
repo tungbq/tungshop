c7df88b35a6f04a6d277b93647f6490697df5bd6
Tung Bui <tungbq.info@gmail.com>
2021-05-22 14:17:39 +0700

    Chap 12: Images upload config endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
+
+import path from 'path'
 import products from './data/products.js'
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
@@ -17,13 +20,17 @@ const app = express()
 
 app.use(express.json())
 
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
