f076fbfcb8cc784e3e29739b1be57b29516dca30
Tung Bui <tungbq.info@gmail.com>
2021-05-29 12:45:22 +0700

    Chap 15: Prepare for deloyment

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,17 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
