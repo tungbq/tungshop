55d2fa2d4cf10bc0f866f3f56284f946540f9fdf
Tung Bui <tungbq.info@gmail.com>
2021-08-24 21:50:00 +0700

    Update header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Proshop</Navbar.Brand>
+            <Navbar.Brand>Tung Shop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
