e7158de1f6fa304a5248058e71d2300f54db0487
Tung Bui <tungbq.info@gmail.com>
2021-08-24 22:26:35 +0700

    Update header and footer UI

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        Copyright &copy; Proshop
+                        Copyright &copy; Tung shop
                     </Col>
                 </Row>
             </Container>
