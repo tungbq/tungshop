f3793b16d72538b26858c9ffda8ee2e3b06d67ab
Tung Bui Quang <85242618+tungbq@users.noreply.github.com>
2021-09-05 23:27:59 +0700

    [Place Order] Stop sending image content in order action (#16)
    * [Place Order] Stop sending image content in order action
    
    * Remove unecessary log

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -9,7 +9,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         payload: {
             product: data._id,
             name: data.name,
-            image: data.image,
+            // image: data.image,
             price: data.price,
             countInStock: data.countInStock,
             qty,
